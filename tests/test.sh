02a2e460e54ce581a192f3d720f6de4cbaf1ec25
softprops <d.tangren@gmail.com>
2019-03-14 22:11:38 -0400

    invoke local support

--- tests/test.sh
+++ tests/test.sh
@@ -46,23 +46,45 @@ DIST=$(cd $HERE/..; echo $PWD)
 
 cd ${HERE}/test-func
 
+echo npm i -D $DIST --silent
 # install build deps
-npm i -D serverless --silent
-assert_success "it installs" npm i -D $DIST --silent
+assert_success "it installs with npm" \
+    npm i -D $DIST --silent
 
 # integration test `package` command
-assert_success "it packages" npx serverless package
+assert_success "it packages with serverless" \
+    npx serverless package
 
 # verify packaged artifact by invoking it using the lambdaci "provided" docker image
 unzip -o  \
     target/lambda/release/test-func.zip \
     -d /tmp/lambda > /dev/null 2>&1 && \
   docker run \
-    -i -e DOCKER_LAMBDA_USE_STDIN=1 \
-    --rm \
+    -i --rm \
+    -e DOCKER_LAMBDA_USE_STDIN=1 \
     -v /tmp/lambda:/var/task \
-    lambci/lambda:provided < test-event.json | grep -v RequestId | grep -v '^\W*$' > test-out.log
+    lambci/lambda:provided \
+    < test-event.json \
+    | grep -v RequestId \
+    | grep -v '^\W*$' \
+    > test-out.log
 
-assert_success "when invoked, it produces expected output" diff test-event.json test-out.log
+assert_success "when invoked, it produces expected output" \
+    diff test-event.json test-out.log
+
+# integration test local invocation
+assert_success "it supports serverless local invocation" \
+    $(npx serverless invoke local -f hello -d '{"baz":"boom"}' \
+     | grep -v Serverless \
+     | grep -v RequestId \
+     | grep -v '^\W*$' \
+     | grep -v " release " \
+     | grep -v "adding: bootstrap" \
+     > local-out.log)
+
+assert_success "when serverless invokes locally, it produces expected output" \
+    diff test-local.json local-out.log
+
+#rm test-out.log
 
 end_tests
